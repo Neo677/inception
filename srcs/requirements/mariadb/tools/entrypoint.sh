fea8a7aa6b7cf6c7cb5464e7eb5fb16943e76117
tom_doe <111650030+Neo677@users.noreply.github.com>
2025-06-29 21:01:13 +0200

    fix
    second user for the wordpress

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,19 +3,19 @@ set -e
 
 echo "---- Initialisation de la base de donnees MariaDB ----"
 
-# Lancer temporairement MySQL sans accès réseau
-echo "⏳ Demarrage temporaire de MariaDB pour configuration..."
+# Lancer temporairement MySQL sans acces reseau
+echo "Demarrage temporaire de MariaDB pour configuration..."
 mysqld_safe --skip-networking &
 sleep 5
 
 until mysqladmin ping --silent -u root; do
-    echo "⏳ Attente du demarrage de MySQL..."
+    echo "Attente du demarrage de MySQL..."
     sleep 1
 done
 
-# Création initiale
+# Creation initiale
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-	echo "✅ Initialisation de la base MySQL..."
+	echo "Initialisation de la base MySQL..."
 	mysql -u root <<-EOSQL
 		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 		FLUSH PRIVILEGES;
@@ -25,11 +25,11 @@ if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 		FLUSH PRIVILEGES;
 	EOSQL
 else
-	echo "✅ Base deja initialisee."
+	echo "Base deja initialisee."
 fi
 
-# Arrêt du MariaDB temporaire
+# Arret du MariaDB temporaire
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-echo "✅ Base de donnees prete, lancement de mysqld..."
+echo "Base de donnees prete, lancement de mysqld..."
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,52 +2,63 @@
 
 set -e
 
-# ➤ Attente que MariaDB soit prets
+# Attente que MariaDB soit prete
 echo "Attente de MariaDB ($MYSQL_HOST)..."
-until mysqladmin --silent ping -h "$MYSQL_HOST" --silent; do
-    sleep 1
+until mysqladmin ping -h"$MYSQL_HOST" --silent; do
+	sleep 1
 done
-echo "MariaDB est pret !"
+echo "MariaDB prete !"
 
-# ➤ Telechargement de WordPress si pas encore prsent
+# Telechargement de WordPress si pas deja present
 if [ ! -f /var/www/html/wp-load.php ]; then
-    echo "  Telechargement de WordPress..."
-    wget https://wordpress.org/latest.tar.gz
-    tar -xzf latest.tar.gz
-    mv wordpress/* /var/www/html/
-    rm -rf latest.tar.gz wordpress
-    echo "WordPress telecharge"
+	echo "Telechargement de WordPress..."
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzf latest.tar.gz
+	mv wordpress/* /var/www/html/
+	rm -rf latest.tar.gz wordpress
+	echo "WordPress telecharge !"
 fi
 
 cd /var/www/html
 
-# ➤ Génération automatique du wp-config.php
+# Creation automatique du wp-config.php
 if [ ! -f wp-config.php ]; then
-    echo "Creation de wp-config.php..."
-    wp config create \
-        --dbname="$MYSQL_DATABASE" \
-	--dbuser="$MYSQL_USER" \
-	--dbpass="$MYSQL_PASSWORD" \
-	--dbhost="$MYSQL_HOST" \
-	--path='/var/www/html' \
-	--allow-root
-   echo "wp-config.php pret !"
+	echo "Creation de wp-config.php..."
+	wp config create \
+		--dbname="$MYSQL_DATABASE" \
+		--dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" \
+		--dbhost="$MYSQL_HOST" \
+		--path='/var/www/html' \
+		--allow-root
+	echo "wp-config.php cree !"
 fi
 
+# Installation de WordPress si non deja installee
 if ! wp core is-installed --allow-root; then
-	echo "installation de wordpress..."
+	echo "Installation de WordPress..."
 	wp core install \
 		--url="$DOMAIN_NAME" \
-		--title="inception42" \
+		--title="inception_42" \
 		--admin_user="$WP_ADMIN" \
 		--admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_EMAIL" \
+		--skip-email \
 		--allow-root
-	echo "WordPress installer !"
+	echo "WordPress installe !"
 else
-	echo "WordPress est deja installer"
+	echo "WordPress deja installe"
 fi
 
-echo "Lancement de PHP-FPM"
-exec php-fpm -F
+# Creation d’un second utilisateur si pas deja existant
+if ! wp user get "$WP_USER" --allow-root > /dev/null 2>&1; then
+	echo "Creation du second user ($WP_USER)..."
+	wp user create "$WP_USER" "$WP_USER_EMAIL" \
+		--user_pass="$WP_USER_PASSWORD" \
+		--role=subscriber \
+		--allow-root
+	echo "Utilisateur secondaire cree !"
+fi
 
+echo "Lancement de PHP-FPM..."
+exec php-fpm -Fz`a
